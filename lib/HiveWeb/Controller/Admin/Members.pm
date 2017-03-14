2f19afa51678cd9aed1299e21489aac9dc7a21b7
Greg J Arnold <greg@arnoldassociates.com>
2017-03-13 21:15:26 -0400

    Add a couple columns.

--- lib/HiveWeb/Controller/Admin/Members.pm
+++ lib/HiveWeb/Controller/Admin/Members.pm
@@ -23,11 +23,25 @@ sub view :Local
 		if ($dir ne "ASC" && $dir ne "DESC");
 	my $sorder .= "$order $dir";
 
-	my @members = $c->model('DB::Member')->search({}, {order_by => $sorder});
+	my @members = $c->model('DB::Member')->search({}, { order_by  => $sorder });
+	my @omembers;
+	foreach my $member (@members)
+		{
+		my %m = $member->get_columns();
+		$m{accesses} = $member->search_related('access_logs')->count();
+		my $lat = $member
+			->search_related('access_logs', { granted => 1 }, { order_by => { -desc => [ 'access_time' ] } })
+			->first();
+		if (defined($lat))
+			{
+			$m{last_access_time} = $lat->access_time();
+			}
+		push (@omembers, \%m);
+		}
 	my @groups  = $c->model('DB::MGroup')->search({});
 
 	$c->stash()->{groups}   = \@groups;
-	$c->stash()->{members}  = \@members;
+	$c->stash()->{members}  = \@omembers;
 	$c->stash()->{order}    = $order;
 	$c->stash()->{dir}      = $dir;
 	$c->stash()->{template} = 'admin/members/index.tt';
