b2650a226909289f3801b3d562cf51cb1d2a3ba4
Greg J Arnold <greg@arnoldassociates.com>
2017-04-18 19:16:17 -0400

    Config file!

---
+++ hiveweb.pl
@@ -0,0 +1,12 @@
+	{
+	soda =>
+		{
+		add_amount => 20,
+		cost => 1000,
+		},
+	stripe =>
+		{
+		public_key => 'pk_live_rP6E92Gc8FKq7yOLGk0kTPmi',
+		secret_key => '< put secret key here >',
+		},
+	}
