279b7fdb41a49ffe3b061c444112f17d9b1f7f64
Greg J Arnold <greg@arnoldassociates.com>
2017-12-28 13:48:53 -0500

    Pass back the protected status of curses.

--- lib/HiveWeb/Schema/Result/Curse.pm
+++ lib/HiveWeb/Schema/Result/Curse.pm
@@ -64,10 +64,12 @@ sub TO_JSON
 
 	return
 		{
-		curse_id     => $self->curse_id(),
-		name         => $self->name(),
-		display_name => $self->display_name(),
-		notification => markdown($self->notification_markdown()),
+		curse_id           => $self->curse_id(),
+		name               => $self->name(),
+		display_name       => $self->display_name(),
+		notification       => markdown($self->notification_markdown()),
+		protect_gorup_cast => $self->protect_group_cast() ? \1 : \0,
+		protect_user_cast  => $self->protect_user_cast() ? \1 : \0,
 		};
 	}
 
