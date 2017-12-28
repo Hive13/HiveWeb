ffcae70ea17f8cf3c63e5a84f972882c32de06ce
Greg J Arnold <greg@arnoldassociates.com>
2017-12-28 15:46:15 -0500

    Add cursing members.

--- lib/HiveWeb/Controller/API/Admin/Curses.pm
+++ lib/HiveWeb/Controller/API/Admin/Curses.pm
@@ -1,16 +1,12 @@
 package HiveWeb::Controller::API::Admin::Curses;
 use Moose;
 use namespace::autoclean;
-use Try::Tiny;
-
-use JSON;
-use DateTime;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
 sub index :Path :Args(0)
 	{
-	my ( $self, $c ) = @_;
+	my ($self, $c) = @_;
 
 	my $in      = $c->stash()->{in};
 	my $out     = $c->stash()->{out};
@@ -19,6 +15,56 @@ sub index :Path :Args(0)
 
 	$out->{response} = \1;
 	$out->{curses}   = \@curses;
+	}
+
+sub cast :Local :Args(0)
+	{
+	my ($self, $c) = @_;
+
+	my $in      = $c->stash()->{in};
+	my $out     = $c->stash()->{out};
+
+	my $member = $c->model('DB::Member')->find({ member_id => $in->{member_id} });
+	if (!defined($member))
+		{
+		$out->{response} = \0;
+		$out->{data}     = "Cannot find member " . $in->{member_id};
+		return;
+		}
+	my $curse = $c->model('DB::Curse')->find({ curse_id => $in->{curse_id} });
+	if (!defined($curse))
+		{
+		$out->{response} = \0;
+		$out->{data}     = "Cannot find curse " . $in->{curse_id};
+		return;
+		}
+
+	my $mc = $c->model('DB::MemberCurse')->find(
+		{
+		member_id => $in->{member_id},
+		curse_id  => $in->{curse_id},
+		issued_at => { '<=' => \'now()' },
+		lifted_at => [ { '>=' => \'now()' }, undef ],
+		});
+	if ($mc)
+		{
+		$out->{response}        = \0;
+		$out->{data}            = "That member already has that curse active.";
+		$out->{member_curse_id} = $mc->member_curse_id();
+		return;
+		}
+
+	$mc = $c->model('DB::MemberCurse')->create(
+		{
+		member_id         => $in->{member_id},
+		curse_id          => $in->{curse_id},
+		issuing_member_id => $c->user()->member_id(),
+		issuing_notes     => $in->{notes},
+		}) || die $!;
+
+	$out->{response}        = \1;
+	$out->{data}            = "Curse cast.";
+	$out->{member_curse_id} = $mc->member_curse_id();
 	}
 
 =encoding utf8
