f47d63be6a1b0cdceec4f3feb3eb71bd4b4e1411
Greg J Arnold <greg@arnoldassociates.com>
2017-12-29 11:52:17 -0500

    Start being able to curse an entire group.

--- lib/HiveWeb/Controller/API/Admin/Curses.pm
+++ lib/HiveWeb/Controller/API/Admin/Curses.pm
@@ -2,6 +2,8 @@ package HiveWeb::Controller::API::Admin::Curses;
 use Moose;
 use namespace::autoclean;
 
+use Try::Tiny;
+
 BEGIN { extends 'Catalyst::Controller'; }
 
 sub index :Path :Args(0)
@@ -21,15 +23,44 @@ sub cast :Local :Args(0)
 	{
 	my ($self, $c) = @_;
 
-	my $in      = $c->stash()->{in};
-	my $out     = $c->stash()->{out};
+	my $in        = $c->stash()->{in};
+	my $out       = $c->stash()->{out};
+	my $member_id = $in->{member_id};
+	my $mgroup_id = $in->{mgroup_id};
 
-	my $member = $c->model('DB::Member')->find({ member_id => $in->{member_id} });
-	if (!defined($member))
+	my $members;
+
+	# Wonder if Perl has an XOR...
+	if ((!$member_id && !$mgroup_id) || ($member_id && $mgroup_id))
 		{
 		$out->{response} = \0;
-		$out->{data}     = "Cannot find member " . $in->{member_id};
+		$out->{data}     = "You must specify either a member or a group.";
 		return;
+		}
+
+	if ($member_id)
+		{
+		my $member = $c->model('DB::Member')->find({ member_id => $member_id });
+		if (!defined($member))
+			{
+			$out->{response} = \0;
+			$out->{data}     = "Cannot find member " . $member_id;
+			return;
+			}
+		$members = [ $member ];
+		}
+	else
+		{
+		my @members;
+		my $mgroup = $c->model('DB::MGroup')->find({ mgroup_id => $mgroup_id });
+		if (!defined($mgroup))
+			{
+			$out->{response} = \0;
+			$out->{data}     = "Cannot find group " . $mgroup_id;
+			return;
+			}
+		@members = $mgroup->member_mgroups()->search_related('member', {})->all();
+		$members = \@members;
 		}
 	my $curse = $c->model('DB::Curse')->find({ curse_id => $in->{curse_id} });
 	if (!defined($curse))
@@ -39,32 +70,44 @@ sub cast :Local :Args(0)
 		return;
 		}
 
-	my $mc = $c->model('DB::MemberCurse')->find(
+	$out->{response} = \1;
+	$out->{data}     = "Curse cast.";
+	try
 		{
-		member_id => $in->{member_id},
-		curse_id  => $in->{curse_id},
-		issued_at => { '<=' => \'now()' },
-		lifted_at => [ { '>=' => \'now()' }, undef ],
-		});
-	if ($mc)
-		{
-		$out->{response}        = \0;
-		$out->{data}            = "That member already has that curse active.";
-		$out->{member_curse_id} = $mc->member_curse_id();
-		return;
+		$c->model('DB')->txn_do(sub
+			{
+			foreach my $member (@$members)
+				{
+				my $mc = $c->model('DB::MemberCurse')->find(
+					{
+					member_id => $member->member_id(),
+					curse_id  => $in->{curse_id},
+					issued_at => { '<=' => \'now()' },
+					lifted_at => [ { '>=' => \'now()' }, undef ],
+					});
+
+				if ($mc)
+					{
+					$out->{data} =  "A member already has that curse active.  Can't handle this yet.";
+					die;
+					}
+
+				$mc = $c->model('DB::MemberCurse')->create(
+					{
+					member_id         => $member->member_id(),
+					curse_id          => $in->{curse_id},
+					issuing_member_id => $c->user()->member_id(),
+					issuing_notes     => $in->{notes},
+					}) || die $!;
+				}
+			});
 		}
-
-	$mc = $c->model('DB::MemberCurse')->create(
+	catch
 		{
-		member_id         => $in->{member_id},
-		curse_id          => $in->{curse_id},
-		issuing_member_id => $c->user()->member_id(),
-		issuing_notes     => $in->{notes},
-		}) || die $!;
-
-	$out->{response}        = \1;
-	$out->{data}            = "Curse cast.";
-	$out->{member_curse_id} = $mc->member_curse_id();
+		$out->{data} = Data::Dumper::Dumper($_)
+			if ($out->{data} eq 'Curse cast.');
+		$out->{response} = \0;
+		}
 	}
 
 =encoding utf8
