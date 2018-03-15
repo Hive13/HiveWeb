65b268133564ace97607c8ea65984506c4fcaff5
Greg J Arnold <greg@arnoldassociates.com>
2018-03-15 10:40:29 -0400

    Tweak.  Resolves #98.

--- lib/HiveWeb/Controller/Member.pm
+++ lib/HiveWeb/Controller/Member.pm
@@ -200,18 +200,6 @@ sub requests :Local :Args(0)
 		});
 	}
 
-
-=head1 AUTHOR
-
-Greg Arnold
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
-
 __PACKAGE__->meta->make_immutable;
 
 1;
