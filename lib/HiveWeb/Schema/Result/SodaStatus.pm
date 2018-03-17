238ca6cf342f495fe82aa0ee52dfddde4a328ed2
Greg J Arnold <greg@arnoldassociates.com>
2018-03-17 19:28:56 -0400

    Schema for the soda table.

---
+++ lib/HiveWeb/Schema/Result/SodaStatus.pm
@@ -0,0 +1,48 @@
+use utf8;
+package HiveWeb::Schema::Result::SodaStatus;
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+__PACKAGE__->load_components(qw{ UUIDColumns });
+__PACKAGE__->table('soda_status');
+
+__PACKAGE__->add_columns(
+  'soda_id',
+  { data_type => 'uuid', is_nullable => 0, size => 16 },
+  'name',
+  { data_type => 'character varying', is_nullable => 0, size => 32 },
+  'diet',
+  { data_type => 'boolean', is_nullable => 0, default_value => 'f' },
+  'sold_out',
+  { data_type => 'boolean', is_nullable => 0, default_value => 'f' },
+	'slot_number',
+  { data_type => 'integer', is_nullable => 0 },
+);
+
+__PACKAGE__->set_primary_key('soda_id');
+__PACKAGE__->uuid_columns('soda_id');
+__PACKAGE__->resultset_attributes( { order_by => ['slot_number'] } );
+
+__PACKAGE__->meta->make_immutable;
+
+sub TO_JSON
+	{
+	my $self = shift;
+
+	return
+		{
+		soda_id     => $self->soda_id(),
+		name        => $self->name(),
+		diet        => $self->diet() ? \1 : \0,
+		sold_out    => $self->sold_out() ? \1 : \0,
+		slot_number => $self->slot_number(),
+		};
+	}
+
+1;
