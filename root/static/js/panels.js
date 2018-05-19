fc556701cb3b6bf302ccb1182a2cde55c06fa8b4
Greg J Arnold <greg@arnoldassociates.com>
2018-05-19 15:03:19 -0400

    More work on styling.

--- root/static/js/panels.js
+++ root/static/js/panels.js
@@ -60,21 +60,20 @@ function display_temp_data(data, $temp_panel)
 
 function display_soda_data(data, $temp_panel)
 	{
-	var $temp_div = $temp_panel.find("h3.soda + div"), html = "", i, soda;
+	var $temp_div = $temp_panel.find("h3.soda + div"), html = "<div class=\"row\">", i, soda;
 
 	for (i = 0; i < data.sodas.length; i++)
 		{
 		soda = data.sodas[i];
-		html += "<span class=\"label";
+		html += "<div class=\"col-xs-12 col-md-6 col-lg-4\"><span class=\"label";
 		if (soda.sold_out)
 			html += " label-danger\" title=\"Sold Out";
 		else
 			html += " label-success\" title=\"In Stock";
-		html += "\">" + soda.name + "</span><br />";
+		html += "\">" + soda.name + "</span></div>";
 		}
 
-	html += "</table>";
-
+	html += "</div>";
 	$temp_div.html(html);
 	}
 
