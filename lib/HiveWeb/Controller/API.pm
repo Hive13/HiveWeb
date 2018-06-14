93aa07902297e2c5901b33835d43ddc307ce1f5b
Greg J Arnold <greg@arnoldassociates.com>
2018-06-13 23:21:53 -0400

    Start uploading photo.

--- lib/HiveWeb/Controller/API.pm
+++ lib/HiveWeb/Controller/API.pm
@@ -290,6 +290,50 @@ sub access :Local
 		}
 	}
 
+sub photo :Local :Args(0)
+	{
+	my ( $self, $c ) = @_;
+
+	my $in        = $c->stash()->{in};
+	my $out       = $c->stash()->{out};
+
+	my $image = $c->request()->upload('photo');
+	if (!$image)
+		{
+		$out->{response} = \0;
+		$out->{data}     = 'Cannot find image data.';
+		return;
+		}
+
+	$out->{response} = \1;
+	$out->{data}     = 'Picture uploaded.';
+	try
+		{
+		$c->model('DB')->txn_do(sub
+			{
+			my $img_data = $image->slurp();
+			my $im = Image::Magick->new() || die $!;
+			$im->BlobToImage($img_data);
+			$im->Resize(geometry => '100x100');
+			my $thumb_data = ($im->ImageToBlob())[0];
+
+			my $db_image = $c->model('DB::Image')->create(
+				{
+				image        => $img_data,
+				thumbnail    => $thumb_data,
+				content_type => $image->type(),
+				}) || die $!;
+			$out->{image_id} = $db_image->image_id();
+			});
+		}
+	catch
+		{
+		delete($out->{image_id});
+		$out->{response} = \0;
+		$out->{data}     = 'Could not upload image: ' . $_;
+		};
+	}
+
 =encoding utf8
 
 =head1 AUTHOR
