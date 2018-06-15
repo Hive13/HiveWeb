980d3fdd77bbb803e1de0c877300ba267ba4ab26
Greg J Arnold <greg@arnoldassociates.com>
2018-06-15 13:01:36 -0400

    Move photo upload to the image controller.

--- lib/HiveWeb/Controller/API/Image.pm
+++ lib/HiveWeb/Controller/API/Image.pm
@@ -11,7 +11,7 @@ sub auto :Private
 	{
 	my ($self, $c) = @_;
 
-	my $image_id    = $c->stash()->{in}->{image_id};
+	my $image_id    = $c->stash()->{in}->{image_id} || return;
 	my $image       = $c->model('DB::Image')->find($image_id) || die 'Invalid image ID';
 	my $attachments = $image->attached_to();
 
@@ -25,6 +25,50 @@ sub auto :Private
 		}
 
 	$c->stash({ image => $image });
+	}
+
+sub upload :Local :Args(0)
+	{
+	my ($self, $c) = @_;
+
+	my $in    = $c->stash()->{in};
+	my $out   = $c->stash()->{out};
+	my $image = $c->request()->upload('photo');
+
+	if (!$image)
+		{
+		$out->{response} = \0;
+		$out->{data}     = 'Cannot find image data.';
+		return;
+		}
+
+	$out->{response} = \1;
+	$out->{data}     = 'Picture uploaded.';
+	try
+		{
+		$c->model('DB')->txn_do(sub
+			{
+			my $img_data = $image->slurp();
+			my $im = Image::Magick->new() || die $!;
+			$im->BlobToImage($img_data);
+			$im->Resize(geometry => '100x100');
+			my $thumb_data = ($im->ImageToBlob())[0];
+
+			my $db_image = $c->model('DB::Image')->create(
+				{
+				image        => $img_data,
+				thumbnail    => $thumb_data,
+				content_type => $image->type(),
+				}) || die $!;
+			$out->{image_id} = $db_image->image_id();
+			});
+		}
+	catch
+		{
+		delete($out->{image_id});
+		$out->{response} = \0;
+		$out->{data}     = 'Could not upload image: ' . $_;
+		};
 	}
 
 sub rotate :Local :Args(0)
