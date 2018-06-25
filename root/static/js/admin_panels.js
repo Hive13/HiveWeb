c636f02d1d57ec1aa0a475a4169ace25ff984b7b
Greg J Arnold <greg@arnoldassociates.com>
2018-06-25 10:30:23 -0400

    Add in link and show dialogue.

--- root/static/js/admin_panels.js
+++ root/static/js/admin_panels.js
@@ -73,10 +73,42 @@ function display_pending_applications(data, $panel, odata)
 			html += "<li><a class=\"anchor-style view-signed-form\" id=\"" + app.form_id + "\">View the signed form.</a></li>";
 		if (app.topic_id)
 			html += "<li><a href=\"https://groups.google.com/a/hive13.org/forum/#!topic/leadership/" + app.topic_id + "\" target=\"_blank\">View the discussion thread.</a></li>";
+		html += "<li><a class=\"anchor-style finalize-application\">Mark this application as finished.</a></li>";
 		html += "</ul>";
 		}
 
 	$panel.find(".panel-body").html(html);
+	$panel.find("a.finalize-application").click(function ()
+		{
+		var application_id = $(this).closest("ul.application").attr("id");
+		var $dialogue =
+			$([
+			"<div class=\"modal fade picture-dialogue\" tabIndex=\"-1\" role=\"dialog\">",
+				"<div class=\"modal-dialog\" role=\"document\">",
+					"<div class=\"modal-content\">",
+						"<div class=\"modal-header\">",
+							"<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\" title=\"Close\"><span aria-hidden=\"true\">&times;</span></button>",
+							"<h3 class=\"modal-title\">Select Result</h3>",
+						"</div>",
+						"<div class=\"modal-body u-text-center\">",
+							"<select>",
+								"<option value=\"accepted\">Accepted</option>",
+								"<option value=\"rejected\">Rejected</option>",
+								"<option value=\"withdrew\">Withdrew</option>",
+								"<option value=\"expired\">Expired</option>",
+							"</select>",
+						"</div>",
+						"<div class=\"modal-footer\">",
+							"<button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Cancel</button>",
+							"<button type=\"button\" class=\"btn btn-primary accept\" disabled>Submit</button>",
+						"</div>",
+					"</div>",
+				"</div>",
+			"</div>"
+			].join(""));
+
+		$dialogue.modal("show");
+		});
 	$panel.find("a.show-picture").click(function()
 		{
 		var picture_id = $(this).attr("id");
