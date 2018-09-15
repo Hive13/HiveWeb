2277cec38876fe58dacad199f66c8d9c573d993f
Greg J Arnold <greg@arnoldassociates.com>
2018-09-15 18:42:21 -0400

    First crack at deploy script.

---
+++ bin/deploy.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+use aliased 'DBIx::Class::DeploymentHandler' => 'DH';
+
+use FindBin;
+use lib $FindBin::Bin . '/../lib';
+use HiveWeb;
+use HiveWeb::Schema;
+
+my $c           = HiveWeb->new || die $!;
+my $config      = $c->config();
+my $schema = HiveWeb::Schema->connect($config->{"Model::DB"}->{connect_info}) || die $!;
+
+my $dh = DH->new(
+	{
+	schema              => $schema,
+	script_directory    => "$FindBin::Bin/../dbicdh",
+	databases           => 'PostgreSQL',
+	sql_translator_args => { add_drop_table => 0 },
+	});
+
+$dh->prepare_install();
+$dh->install();
