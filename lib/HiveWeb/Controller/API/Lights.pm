9383c131641d342e57c6d364f9e852a0bf550a2c
Greg J Arnold <greg@arnoldassociates.com>
2018-09-25 15:33:11 -0400

    Start sending updates to lights.

--- lib/HiveWeb/Controller/API/Lights.pm
+++ lib/HiveWeb/Controller/API/Lights.pm
@@ -1,8 +1,15 @@
 package HiveWeb::Controller::API::Lights;
 use Moose;
 use namespace::autoclean;
+use Try::Tiny;
 
 BEGIN { extends 'Catalyst::Controller' }
+
+
+sub send_updates
+	{
+	my ($self, $c) = shift;
+	}
 
 sub auto :Private
 	{
@@ -78,6 +85,8 @@ sub off :Local :Args(0)
 
 	$bulb_rs->update({ value => 0 });
 
+	$self->send_updates($c);
+
 	$out->{response} = \1;
 	}
 
@@ -90,6 +99,8 @@ sub on :Local :Args(0)
 	my $bulb_rs = $current->bulb_presets() || return;
 
 	$bulb_rs->update({ value => 1 });
+
+	$self->send_updates($c);
 
 	$out->{response} = \1;
 	}
@@ -120,9 +131,41 @@ sub load :Local :Args(0)
 			}
 		});
 
-	$bulb_rs->update({ value => 1 });
+	$self->send_updates($c);
 
 	$out->{response} = \1;
+	}
+
+sub set :Local :Args(0)
+	{
+	my ($self, $c) = @_;
+
+	my $out      = $c->stash()->{out};
+	my $in       = $c->stash()->{in};
+	my $current  = $c->model('DB::LampPreset')->find({ name => 'current' }) || return;
+	my $sets     = $in->{bulbs};
+
+	$out->{response} = \1;
+	try
+		{
+		$c->model('DB')->txn_do(sub
+			{
+			foreach my $bulb_id (keys %$sets)
+				{
+				$current->update_or_create_related('bulb_presets',
+					{
+					bulb_id => $bulb_id,
+					value   => ($sets->{$bulb_id} ? 't' : 'f'),
+					}) || die $!;
+				}
+			});
+		}
+	catch
+		{
+		$out->{response} = \0;
+		$out->{data}     ='Cannot set current: ' . $_;
+		};
+	$self->send_updates($c);
 	}
 
 sub save :Local :Args(0)
