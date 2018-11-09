9cda6d8d114869f57523fde70f3021ff0b276265
Greg J Arnold <greg@arnoldassociates.com>
2018-11-09 10:27:32 -0500

    Start showing profile.

--- root/static/js/admin.js
+++ root/static/js/admin.js
@@ -24,7 +24,16 @@ function view_profile(member_id)
 	var s = function(data)
 		{
 		var member = data.member;
-		var $dialogue = $([
+		var phone, $dialogue, html;
+
+		if (member.phone)
+			{
+			phone = member.phone + "";
+			phone = "(" + phone.substr(0, 3) + ") " + phone.substr(3, 3) + "-" + phone.substr(6);
+			}
+
+		html =
+			[
 			"<div class=\"modal fade\" tabIndex=\"-1\" role=\"dialog\">",
 				"<div class=\"modal-dialog\" role=\"document\">",
 					"<div class=\"modal-content\">",
@@ -33,6 +42,25 @@ function view_profile(member_id)
 							"<h3 class=\"modal-title\">Profile for " + member.fname + " " + member.lname + "</h3>",
 						"</div>",
 						"<div class=\"modal-body\">",
+							"<table class=\"hive-profile-table\">",
+								"<tr>",
+									"<td>E-mail Address</td>",
+									"<td>" + member.email + "</td>",
+								"</tr>",
+								"<tr>",
+									"<td>Handle/Nickname</td>",
+									"<td>" + member.handle + "</td>",
+								"</tr>"
+			];
+		if (phone)
+			html.push(
+								"<tr>",
+									"<td>Phone Number</td>",
+									"<td>" + phone + "</td>",
+								"</tr>"
+			);
+		html.push(
+							"</table>",
 						"</div>",
 						"<div class=\"modal-footer\">",
 							"<button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>",
@@ -40,7 +68,8 @@ function view_profile(member_id)
 					"</div>",
 				"</div>",
 			"</div>"
-			].join(''));
+		);
+		$dialogue = $(html.join(""));
 		$dialogue.modal("show");
 		};
 
