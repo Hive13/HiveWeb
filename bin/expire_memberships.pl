e99f5cdd183d185f36e2918e8d757581d8b78e75
Greg J Arnold <greg@arnoldassociates.com>
2019-02-11 15:54:28 -0500

    Get candidate members.

--- bin/expire_memberships.pl
+++ bin/expire_memberships.pl
@@ -10,3 +10,21 @@ use HiveWeb::Schema;
 my $config = HiveWeb->config();
 my $schema = HiveWeb::Schema->connect($config->{"Model::DB"}->{connect_info}) || die $!;
 
+my $begin_date = 35;
+my $group_name = 'members';
+
+my $pay_date_query = $schema->resultset('Payment')->search(
+	{ payment_date => { '<=' => \"now() - interval '$begin_date days'" } },
+	{ alias => 'payment' })->get_column('member_id')->as_query() || die $!;
+my $member_query = $schema->resultset('MemberMgroup')->search(
+	{ 'mgroup.name' => $group_name },
+	{ alias => 'mem_group', join => 'mgroup' }
+	)->get_column('member_id')->as_query() || die $!;
+my $candidates = $schema->resultset('Member')->search(
+	{
+	paypal_email     => [ { 'like' => '%@%' }, undef ],
+	linked_member_id => undef,
+	member_id        => { '-not_in' => $pay_date_query, '-in' => $member_query },
+	}) || die $!;
+
+my @a = $candidates->all();
