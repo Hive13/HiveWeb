dcc5cb272531ac206d53cb11642996804058506e
Greg J Arnold <greg@arnoldassociates.com>
2019-03-04 18:46:47 -0500

    Add in move function.

--- lib/HiveWeb/Controller/API/Panel.pm
+++ lib/HiveWeb/Controller/API/Panel.pm
@@ -10,6 +10,59 @@ sub index :Path :Args(0)
 	my ($self, $c) = @_;
 	}
 
+sub move :Local :Args(0)
+	{
+	my ($self, $c) = @_;
+
+	my $out       = $c->stash()->{out};
+	my $in        = $c->stash()->{in};
+	my $user      = $c->user();
+	my $direction = lc($in->{direction} || 'left');
+	my $member_id = $user->member_id();
+	my $panels_rs = $c->model('DB::PanelMember')->search({}, { bind => [ $member_id ] });
+
+	$c->model('DB')->txn_do(sub
+		{
+		my $prev;
+		while (my $panel = $panels_rs->next())
+			{
+			if (defined(my $perm = $panel->permissions()))
+				{
+				if ($perm eq '' || $perm eq 'user')
+					{
+					next if (!$user);
+					}
+				else
+					{
+					next if (!$c->check_user_roles($perm));
+					}
+				}
+			if ( ($direction eq 'left' && $panel->panel_id() eq $in->{panel_id})
+			  || ($direction eq 'right' && $prev && $prev->panel_id() eq $in->{panel_id}) )
+				{
+				return if (!$prev);
+				$c->model('DB::MemberPanel')->update_or_create(
+					{
+					member_id  => $member_id,
+					panel_id   => $panel->panel_id(),
+					sort_order => $prev->sort_order(),
+					}) || die $!;
+				$c->model('DB::MemberPanel')->update_or_create(
+					{
+					member_id  => $member_id,
+					panel_id   => $prev->panel_id(),
+					sort_order => $panel->sort_order(),
+					}) || die $!;
+				$out->{response} = \1;
+				return;
+				}
+			$prev = $panel;
+			}
+		});
+
+	$out->{response} = \1;
+	}
+
 sub hide :Local :Args(0)
 	{
 	my ($self, $c) = @_;
@@ -20,7 +73,7 @@ sub hide :Local :Args(0)
 
 	$c->model('DB')->txn_do(sub
 		{
-		my $mp = $panel->find_or_create_related('member_panels',
+		my $mp = $panel->update_or_create_related('member_panels',
 			{
 			member_id => $c->user()->member_id(),
 			visible   => 'f',
