f9a837fe57ff0c496b175b23a2c1adf257c1584b
Greg J Arnold <greg@arnoldassociates.com>
2019-05-11 21:42:37 -0400

    Upgrade from DBI v.11.

--- dbicdh/PostgreSQL/upgrade/11-12/001-auto.sql
+++ dbicdh/PostgreSQL/upgrade/11-12/001-auto.sql
@@ -1,4 +1,4 @@
--- Convert schema '/home/greg/git/HiveWeb/bin/../dbicdh/_source/deploy/10/001-auto.yml' to '/home/greg/git/HiveWeb/bin/../dbicdh/_source/deploy/12/001-auto.yml':;
+-- Convert schema '/home/greg/git/HiveWeb/bin/../dbicdh/_source/deploy/11/001-auto.yml' to '/home/greg/git/HiveWeb/bin/../dbicdh/_source/deploy/12/001-auto.yml':;
 
 ;
 BEGIN;
